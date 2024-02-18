b7b9be8b3a3d40bd9150cf401499f6e8b5d2383c
Muhammad Yousef <muhammad.yousef7489@gmail.com>
2024-02-18 04:34:24 +0200

    removing the gspread fuctionality, and replacing it with database using sqlalchmey

--- dubizzle.py
+++ dubizzle.py
@@ -1,11 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime as dt 
-import gspread
+from datetime import timedelta
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 
-gc = gspread.service_account('bot_creds.json')
-sh = gc.open('Pythoner')
 
+engine = create_engine('sqlite:///data.db')
+
+Base = declarative_base()
+
+class property_ad(Base): 
+    __tablename__ = 'users'
+    id = Column(Integer,primary_key=True)
+    scrapping_date =Column(String)
+    ad_date_finder=Column(String)
+    title_finder=Column(String)
+    price_finder=Column(String)
+    seller_finder=Column(String)
+    city_area_finder=Column(String)
+    ad_id_finder=Column(String)
+    space_sqm_finder=Column(String)
+    prpty_type_finder=Column(String)
+    bed_room_finder=Column(String)
+    bath_rooms_finder=Column(String)
+    furnished_finder=Column(String)
+    floor_finder=Column(String)
+    payment_method_finder=Column(String)
+
+Base.metadata.create_all(engine)
+
+
+Session = sessionmaker(bind =engine)
+session = Session()
+
+
+
+def ad_date_converter(ad_date_finder):
+    """ This function takes the string of ad date in the website, compare it to scrapping date, and convert it to date format"""
+    splitted = ad_date_finder.split()
+    if splitted[2] == "أيام" or 'يوم':
+        result = dt.today()-timedelta(days=int(splitted[1]))
+    elif splitted[2]== 'أسبوع' or 'أسابيع':
+        result = dt.today()-timedelta(weeks=int(splitted[1]))
+    elif splitted[2] == 'ساعات' or 'ساعة':
+        result = dt.today() - timedelta(hours=splitted[2])
+    elif splitted[2] == 'دقائق':
+        result = dt.today() - timedelta(minutes=splitted[2])
+    return result
 
 
 def finder(tag_,cls): 
@@ -26,7 +70,6 @@ def nexter(tag_,text_):
 
 h = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'}
 website = 'https://www.dubizzle.com.eg'
-# url = 'https://www.dubizzle.com.eg/properties/apartments-duplex-for-sale/alexandria/?page='
 url = 'https://www.dubizzle.com.eg/properties/apartments-duplex-for-rent/?page='
 
 ad_links = []
@@ -56,8 +99,11 @@ for page_num in range(1,188):
         s = BeautifulSoup(r,'html.parser')
         
         try:
+                
             ad_date_finder = s.find('div',class_='_1075545d d059c029 _858a64cf').find_next('div',class_='_1075545d e3cecb8b _5f872d11').find_next('span',class_='_6d5b4928').find_next('span').get_text()
-        
+
+            ad_date = ad_date_converter(ad_date_finder)
+
             title_finder = finder('h1','a38b8112')
         
             print(f"Now Scrapping: {title_finder}")
@@ -84,27 +130,29 @@ for page_num in range(1,188):
 
             payment_method_finder = nexter('span','طريقة الدفع')
 
-            
+            property = property_ad(
+                scrapping_date = str(dt.today()),
+                ad_date_finder = ad_date,
+                title_finder=  title_finder,
+                price_finder = price_finder,
+                seller_finder= seller_finder,
+                city_area_finder= city_area_finder,
+                ad_id_finder=  ad_id_finder,
+                space_sqm_finder= space_sqm_finder,
+                prpty_type_finder= prpty_type_finder,
+                bed_room_finder=  bed_room_finder,
+                bath_rooms_finder= bath_rooms_finder,
+                furnished_finder=   furnished_finder,
+                floor_finder=  floor_finder,
+                payment_method_finder = payment_method_finder,
+            )
 
-            all_values_row = [
-                str(dt.today()),
-                ad_date_finder,
-                title_finder,
-                price_finder,
-                seller_finder,
-                city_area_finder,
-                ad_id_finder,
-                space_sqm_finder,
-                prpty_type_finder,
-                bed_room_finder,
-                bath_rooms_finder,
-                furnished_finder,
-                floor_finder,
-                payment_method_finder,
-                ]
-                
 
-            sh.worksheet('Sheet6').append_row(all_values_row)
+
+            session.add(property)
+            session.commit()    
         except:
             pass
+
+
 print('Scrapping process has finished.')
